becff649ba70b393f656aa7d709da22401c16934
jchase2 <mr.jdchase@gmail.com>
2021-08-02 14:45:03 -0400

    Implemented grommet ui in serverinfo page.

--- client/src/components/server/Server.tsx
+++ client/src/components/server/Server.tsx
@@ -13,10 +13,10 @@ const Server = (props: any) => {
   const history = useHistory();
   return (
     <Main pad="medium">
-      <Card height="small" width="small" background="light-1">
-        <CardHeader background="dark-1" pad="small">{props.serverData.url}</CardHeader>
+      <Card height="small" width="medium" background="light-1">
+        <CardHeader background="dark-1" pad="small">{props.serverData.name}</CardHeader>
         <CardBody pad="small">
-            <Box>Server: {props.serverData.name}</Box>
+            <Box>Server URL: {props.serverData.url}</Box>
             <Box>
               Server Status: {props.serverData.status === "up" ? "Up" : "Down!"}
             </Box>
@@ -24,7 +24,7 @@ const Server = (props: any) => {
               SSL: {props.serverData.sslStatus === "true" ? "Active" : "Down!"}
             </Box>
         </CardBody>
-        <CardFooter pad={{ horizontal: "small" }} background="light-2" align="center" justify="center">
+        <CardFooter pad="small" background="light-2" align="center" justify="center">
           <Button
             plain={false}
             onClick={() => history.push("/server/" + props.serverData.id)}
--- client/src/components/server/ServerInfo.tsx
+++ client/src/components/server/ServerInfo.tsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getIndServer } from "../../services/api";
+import {
+  Main,
+  Box,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+} from "grommet";
+import { useHistory } from "react-router-dom";
+
 
 const ServerInfo = (props: any) => {
   interface IServer {
@@ -27,15 +38,36 @@ const ServerInfo = (props: any) => {
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  const history = useHistory();
   return (
-    <>
-        Name: {serverData.name}
-        URL: {serverData.url}
-        Status: {serverData.status}
-        SSL: {serverData.sslStatus}
-        SSL Expires: {serverData.sslExpiry}
-        </>
+    <Main align="center" justify="center">
+      <Card height="medium" width="medium" background="light-1">
+        <CardHeader background="dark-1" pad="small">
+          {serverData.name}
+        </CardHeader>
+        <CardBody pad="small">
+          <Box>Server URL: {serverData.url}</Box>
+          <Box>
+            Server Status: {serverData.status === "up" ? "Up" : "Down!"}
+          </Box>
+          <Box>SSL: {serverData.sslStatus === "true" ? "Active" : "Down!"}</Box>
+          <Box> SSL Expires: {serverData.sslExpiry} Days</Box>
+        </CardBody>
+        <CardFooter
+          pad="small"
+          background="light-2"
+          align="center"
+          justify="center"
+        >
+          <Button
+            plain={false}
+            onClick={() => history.push("/server")}
+          >
+            Dashboard
+          </Button>
+        </CardFooter>
+      </Card>
+    </Main>
   );
 };
 
