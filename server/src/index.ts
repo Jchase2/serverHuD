7dde7aabb994be8563a3158074231772aa791ef9
jchase2 <mr.jdchase@gmail.com>
2022-05-23 16:09:41 -0400

    Installed and set up Socket.Io on backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,15 +5,29 @@ import router from './Router/routes';
 import koaBody from 'koa-body';
 import logger from 'koa-logger';
 import cors from '@koa/cors';
+import { Server, Socket } from "socket.io";
+import http from 'http'
 
 const app = new Koa();
 app.use(logger());
-
 app.use(cors());
 app.use(koaBody());
 app.use(router.routes());
 
+const server = http.createServer(app.callback())
+
+const io = new Server(server, {
+  cors: {
+    origin: "localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
 (async () => {
   await sequelize.sync({force: false});
-  app.listen( 3001, () => console.log(`Server running on port: ${3001}`));
+  server.listen( 3001, () => console.log(`Server running on port: ${3001}`));
 })();
+
+io.on('connection', function(socket: Socket){
+  console.log(socket.id)
+})
