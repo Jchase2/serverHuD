f1a76cc10986d40925ba878ec829d638ddc2d60f
jchase2 <mr.jdchase@gmail.com>
2025-04-24 15:11:57 -0400

    Initial scaffolding for nextjs port of serverHuD.

---
+++ next/serverhud/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+};
+
+export default nextConfig;
